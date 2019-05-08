e0966099298cc7cfecaf87393f8c53e28e5ffbd3
Todd <todd@Todds-MacBook-Pro.local>
2019-05-08 14:44:34 -0400

    Fix url routing issue

--- src/js/index.js
+++ src/js/index.js
@@ -98,9 +98,9 @@ const render_core = () => {
 
 render_core();
 var currentpath_whole = window.location.pathname;
-var currentpath_type = window.location.pathname.split('#')[1];
+var currentpath_type = window.location.href.split('#')[1];
 
-if (currentpath_type !== undefined) {
+if (currentpath_type !== undefined || currentpath_type !== '') {
     import('./' + currentpath_type).then(module => {
         module.init_render();
         document.querySelector('.minion-dataload').setAttribute('status', '');
